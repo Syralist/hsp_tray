ad4b48a745d3fe6b91c10f90ac09835e2c824923
Thomas Helmke <thomas.helmke@gmx.de>
2015-03-11 20:30:58 +0100

    reading and parsing status moved to functions added timer

--- hsp_tray.py
+++ hsp_tray.py
@@ -13,55 +13,7 @@ ICON_CLOSED = ''
 STATUS_OPEN = False
 STATUS_MESSAGE = ''
 
-
-def create_menu_item(menu, label, func):
-    item = wx.MenuItem(menu, -1, label)
-    menu.Bind(wx.EVT_MENU, func, id=item.GetId())
-    menu.AppendItem(item)
-    return item
-
-
-class TaskBarIcon(wx.TaskBarIcon):
-    def __init__(self):
-        global ICON_OPEN
-        global ICON_CLOSED
-        global STATUS_OPEN
-        global STATUS_MESSAGE
-        super(TaskBarIcon, self).__init__()
-        # self.set_icon(TRAY_ICON)
-        icon = wx.EmptyIcon()
-        if STATUS_OPEN:
-            icon.CopyFromBitmap(wx.BitmapFromImage(wx.ImageFromStream(StringIO(ICON_OPEN))))
-        else:
-            icon.CopyFromBitmap(wx.BitmapFromImage(wx.ImageFromStream(StringIO(ICON_CLOSED))))
-        # icon.SetWidth(48)
-        # icon.SetHeight(48)
-        print icon
-        self.SetIcon(icon, TRAY_TOOLTIP)
-        self.Bind(wx.EVT_TASKBAR_LEFT_DOWN, self.on_left_down)
-
-    def CreatePopupMenu(self):
-        menu = wx.Menu()
-        create_menu_item(menu, 'Say Hello', self.on_hello)
-        menu.AppendSeparator()
-        create_menu_item(menu, 'Exit', self.on_exit)
-        return menu
-
-    def set_icon(self, path):
-        icon = wx.IconFromBitmap(wx.Bitmap(path))
-        self.SetIcon(icon, TRAY_TOOLTIP)
-
-    def on_left_down(self, event):
-        print 'Tray icon was left-clicked.'
-
-    def on_hello(self, event):
-        print 'Hello, world!'
-
-    def on_exit(self, event):
-        wx.CallAfter(self.Destroy)
-
-
-def main():
+def get_Status():
     global ICON_OPEN
     global ICON_CLOSED
     global STATUS_OPEN
@@ -82,6 +34,58 @@ def main():
     #     print response.json()['icon']['open']
     # else:
     #     print response.json()['icon']['closed']
+
+
+
+def create_menu_item(menu, label, func):
+    item = wx.MenuItem(menu, -1, label)
+    menu.Bind(wx.EVT_MENU, func, id=item.GetId())
+    menu.AppendItem(item)
+    return item
+
+
+class TaskBarIcon(wx.TaskBarIcon):
+    def __init__(self):
+        global ICON_OPEN
+        global ICON_CLOSED
+        global STATUS_OPEN
+        global STATUS_MESSAGE
+        super(TaskBarIcon, self).__init__()
+        self.timer = wx.Timer(self, 100)
+        self.timer.Start(60000)
+        wx.EVT_TIMER(self, 100, self.read_status)
+        self.read_status(0)
+        self.Bind(wx.EVT_TASKBAR_LEFT_DOWN, self.on_left_down)
+
+    def CreatePopupMenu(self):
+        menu = wx.Menu()
+        create_menu_item(menu, 'Say Hello', self.on_hello)
+        menu.AppendSeparator()
+        create_menu_item(menu, 'Exit', self.on_exit)
+        return menu
+
+    def on_left_down(self, event):
+        print 'Tray icon was left-clicked.'
+
+    def on_hello(self, event):
+        print 'Hello, world!'
+
+    def read_status(self, event):
+        get_Status()
+        icon = wx.EmptyIcon()
+        if STATUS_OPEN:
+            icon.CopyFromBitmap(wx.BitmapFromImage(wx.ImageFromStream(StringIO(ICON_OPEN))))
+        else:
+            icon.CopyFromBitmap(wx.BitmapFromImage(wx.ImageFromStream(StringIO(ICON_CLOSED))))
+        print icon
+        self.SetIcon(icon, STATUS_MESSAGE)
+
+
+    def on_exit(self, event):
+        wx.CallAfter(self.Destroy)
+
+
+def main():
 
     app = wx.PySimpleApp()
     TaskBarIcon()
